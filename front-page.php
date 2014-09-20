b698260064b647b149ccce7f94bbcac744467e2d
Schurpf Mac <michael@schurpf.com>
2014-09-20 09:34:24 -0500

    fix comment

--- front-page.php
+++ front-page.php
@@ -1,16 +1,8 @@
 <?php
-
-/**
- * Home Page.
- *
- * @category   Genesis_Sandbox
- * @package    Templates
- * @subpackage Home
- * @author     Travis Smith and Jonathan Perez, for Surefire Themes
- * @license    http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
- * @link       http://schurpf.com/
- * @since      1.0.0
+/*
+Front Page Template
  */
+
 // Force full width page layout
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
